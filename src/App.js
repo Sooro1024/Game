e39564fffcde0add163275cb3c93e624a1e61308
Gor Gevorgyan <soorogorgoyan@gmail.com>
2019-11-22 16:11:56 +0400

    BrouserRouter was chaged to HashRouter for GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Snake } from './components/snake/Snake';
-import { Navigation } from '../src/components/comon/NavBar';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
-import { Game as MatchPic } from './components/matchPic/Game';
-import Puzzle15 from './components/15Puzzle/Puzzle15';
-import { Home } from './components/comon/Home';
+import React from "react";
+import { Snake } from "./components/snake/Snake";
+import { Navigation } from "../src/components/comon/NavBar";
+import { Switch, HashRouter as Router, Route } from "react-router-dom";
+import { Game as MatchPic } from "./components/matchPic/Game";
+import Puzzle15 from "./components/15Puzzle/Puzzle15";
+import { Home } from "./components/comon/Home";
 
 function App() {
   return (
@@ -12,10 +12,10 @@ function App() {
       <Router>
         <Navigation />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/snake' component={Snake} />
-          <Route path='/match-the-picture' component={MatchPic} />
-          <Route path='/puzzle-15' component={Puzzle15} />
+          <Route path="/" exact component={Home} />
+          <Route path="/snake" component={Snake} />
+          <Route path="/match-the-picture" component={MatchPic} />
+          <Route path="/puzzle-15" component={Puzzle15} />
         </Switch>
       </Router>
     </>
--- src/components/comon/NavBar.js
+++ src/components/comon/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -7,40 +7,40 @@ import {
   Nav,
   NavItem,
   NavLink
-} from 'reactstrap';
-import { Link } from 'react-router-dom';
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
   return (
-    <Navbar color='dark' dark expand='md' className='mb-xs-2 mb-md-4'>
-      <NavbarBrand className='text-white' color='light' tag='div'>
-        <Link className='text-white navbar-brand' to='/'>
-          TEST TASK
+    <Navbar color="dark" dark expand="md" className="mb-xs-2 mb-md-4">
+      <NavbarBrand className="text-white" color="light" tag="div">
+        <Link className="text-white navbar-brand" to="/">
+          Games Made With REACT
         </Link>
       </NavbarBrand>
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
-        <Nav className='ml-auto' navbar>
+        <Nav className="ml-auto" navbar>
           <NavItem>
-            <NavLink tag='span'>
-              <Link className='nav-link' to='/puzzle-15'>
+            <NavLink tag="span">
+              <Link className="nav-link" to="/puzzle-15">
                 15 Puzzle
               </Link>
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink tag='span'>
-              <Link className='nav-link' to='/match-the-picture'>
+            <NavLink tag="span">
+              <Link className="nav-link" to="/match-the-picture">
                 Match the picture
               </Link>
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink tag='span'>
-              <Link className='nav-link' to='/snake'>
+            <NavLink tag="span">
+              <Link className="nav-link" to="/snake">
                 Snake
               </Link>
             </NavLink>
